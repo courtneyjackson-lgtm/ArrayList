a436b949068a19123140e190ba6782b3fdcc5ba4
courtneyjackson-lgtm <courtney.jackson@csuglobal.edu>
2026-06-22 01:44:16 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,82 +1,49 @@
 package Student;
 
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
-import java.util.Random; 
 
- 
+public class Main {
 
-public class Main { 
-
- 
-
-	public static void sort(ArrayList <Student> Students) { 
-
-		RollNoSort rollnosort = new RollNoSort(); 
-
-		int j = Students.size(); 
-
-		 
-
-		for (int i = 0; i < j - 1; i++) 
-
-		{ int min_index = i; 
-
-		 
-
-		for (int k = i + 1; k < j; k++) 
-
-			if (rollnosort.compare(Students.get(k), Students.get(min_index)) < 0) 
-
-				min_index = k; 
-
-		Student temp = Students.get(min_index); 
-
-		Students.set(min_index, Students.get(i)); 
-
-		Students.set(i,temp); 
-
-		} 
-
-	} 
-
- 
-
-	public static void main(String[] args) { 
-
-		Random random = new Random(); 
-
-		ArrayList <Student> Students = new ArrayList<>(); 
-
-		for (int i = 0; i < 10; i++) { 
-
-			int rollno = random.nextInt(50); 
-
-			String name = "Name" + i; 
-
-			String address = "Address" + i; 
-
-			Students.add(new Student(rollno, name, address)); 
-
-		} 
-
-		sort(Students); 
-
-	 
-
-		System.out.println("Student that are sorted: "); 
-
-		for (Student s : Students) { 
-
-			System.out.println(s); 
-
-		} 
-
-	} 
-
- 
-
-} 
+	public static void SelectSort(ArrayList <Student> Students) {
+		int j = Students.size();
+		
+		for (int i = 0; i < j - 1; i++) { 
+			int min_index = i;
+		
+		for (int k = i + 1; k < j; k++) {
+			if (Students.get(k).rollno < Students.get(min_index).rollno) { 
+				min_index = k;
+			}
+		}
+		if (min_index != i ) {
+			Student temp = Students.get(i);
+			Students.set(i, Students.get(min_index));
+			Students.set(min_index, temp);
+		}
+	}
+}
+	
+	public static void main(String[] args) {
+		ArrayList <Student> Students = new ArrayList<>();
+		Students.add(new Student(55, "Cathy", "777 Oak Dr"));
+		Students.add(new Student(15, "Carl", "183 Fiber St"));
+		Students.add(new Student(40, "Dave", "829 Cedar St"));
+		Students.add(new Student(25, "Bobby", "397 Pine Dr"));
+		Students.add(new Student(10, "Courtney", "431 Fire Dr"));
+		Students.add(new Student(35, "Mia", "587 Line Ave"));
+		Students.add(new Student(30, "Raven", "209 Jackson Ave"));
+		Students.add(new Student(45, "Michael", "873 South Ave"));
+		Students.add(new Student(50, "Whitney", "554 North Dr"));
+		Students.add(new Student(20, "Kevin", "124 Wood St"));
+		
+		SelectSort(Students);
+		System.out.println("Sorted Names: ");
+		for (Student st : Students) {
+			System.out.println(st);
+		}
+	}
+}
 
 
 
